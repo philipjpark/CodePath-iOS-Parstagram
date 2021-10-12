056fd79dba9dd5fd7f97df41aed0f5290808b579
philipjpark <philipparknyc@gmail.com>
2021-10-12 19:28:47 -0400

    Set up dependencies and Parse server

---
+++ parstagram/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  parstagram
+//
+//  Created by Philip Park on 10/12/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+
+    @IBAction func onSignup(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
