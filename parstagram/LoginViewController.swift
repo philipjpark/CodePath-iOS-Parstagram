a4c0617bd981a276b6f0ecbf728eb7f398fe86d9
philipjpark <philipparknyc@gmail.com>
2021-10-12 21:11:56 -0400

    Signing up and logging in

--- parstagram/LoginViewController.swift
+++ parstagram/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -24,10 +25,32 @@ class LoginViewController: UIViewController {
         
     }
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
 
     @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
+    
     /*
     // MARK: - Navigation
 
